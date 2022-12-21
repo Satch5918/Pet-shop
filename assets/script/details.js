50bf3a4c5de4ed1d2cf084b1eaa869036db52884
LucianaRovere <luciana.rovere96@gmail.com>
2022-12-21 19:20:40 -0300

    XD

--- assets/script/details.js
+++ assets/script/details.js
@@ -4,10 +4,10 @@ let finded;
 let idUrl;
 const contenedor = document.getElementById('contenedor')
 
-fetch('https://amazing-events.onrender.com/api/events')
+fetch('https://mindhub-xj03.onrender.com/api/petshop')
   .then(result => result.json())
   .then(capturarDatos =>{
-    datos = capturarDatos.events
+    datos = capturarDatos
     idUrl = new URLSearchParams(location.search).get('id')
     finded = datos.find(item => item._id == idUrl)
     CardDetails(contenedor,finded)
--- assets/script/farmacia.js
+++ assets/script/farmacia.js
@@ -1,32 +1,23 @@
-import {cards, inputscheck, filtradoPorCategoria, filtradoPorBusqueda, filtrados } from '../../module/funciones.js'
+import {cards, filtradoPorBusqueda} from '../../module/funciones.js'
 const div2 = document.getElementById('divrow1');
-const divcheck = document.getElementById('checkboxs');
-const seccioncheck = document.getElementById('seccion1');
 const search = document.getElementById('search')
 
 let datos;
-let categorias;
-let uncoming;
+let farmacia;
 
-fetch('https://amazing-events.onrender.com/api/events')
+fetch('https://mindhub-xj03.onrender.com/api/petshop')
   .then(result => result.json())
   .then(capturarDatos =>{
-    datos = capturarDatos.events;
-    uncoming = datos.filter( card => card.date >= capturarDatos.currentDate)
-    categorias = uncoming.map(elemento => elemento.category)
-    let mySet = new Set(categorias);
-    categorias = Array.from(mySet);
-    cards(div2,uncoming);
-}).catch( error => {
+    datos = capturarDatos;
+    farmacia = datos.filter( card => card.categoria === "farmacia")
+    cards(div2,farmacia);
+})
+.catch( error => {
   console.log("error:",error);
 });
 
-seccioncheck.addEventListener('change', (e) =>{
-  let filtradoPorBusquedas = filtrados (categorias,uncoming,search)
-  cards(div2,filtradoPorBusquedas)
-})
 
 search.addEventListener('input', () => {
-let filtradoPorBusquedas = filtrados (categorias,uncoming,search)
+let filtradoPorBusquedas = filtradoPorBusqueda (farmacia,search.value)
 cards(div2,filtradoPorBusquedas)
 })
